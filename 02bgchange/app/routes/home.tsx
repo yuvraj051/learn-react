841c0dea7ddd95c09f1d7edf7eb2ea8cb8a5989c
Yuvraj <vyuvraj051@example.com>
2025-06-10 10:34:36 +0530

    setup project with tailwind

---
+++ 02bgchange/app/routes/home.tsx
@@ -0,0 +1,16 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="bg-blend-exclusion text-orange-700">hello</h1>
+    </>
+  );
+}
---
+++ 02bgchange/vite.config.ts
@@ -0,0 +1,7 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+});
